9f1dd45c30d907896a87902f085c1b66f103ede0
Tiago <tvhollowknight@gmail.com>
2024-10-30 15:47:27 -0300

    Greatly improved readability
    The code is now much more concise

--- substrings.rb
+++ substrings.rb
@@ -1,20 +1,14 @@
 def substrings (string, dictionary)
   resulting_hash = {}
-  words_array = string.split(" ")
+  downcase_text = string.downcase
 
-  words_array.each do |word|
-    substrings_found = dictionary.select {|string| word.downcase.include?(string)}
-    substrings_found.each do |substring|
-      if resulting_hash[substring] == nil
-        resulting_hash[substring] = 1
-      else
-        resulting_hash[substring] += 1
-      end
-    end
+  dictionary.each do |word|
+    word_matches = downcase_text.scan(word).length
+    resulting_hash[word] = word_matches unless word_matches == 0
   end
-  p Hash[resulting_hash.sort]
+  Hash[resulting_hash.sort]
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
